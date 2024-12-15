2f4e1c62df2a10371eee5925d0956d528eb4061a
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-12-15 20:37:18 +0100

    Minor changes, license, project renaming.

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2025 Rafael Senties Martinelli.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,20 +34,20 @@ struct VlcContainer{
     libvlc_media_t *media;
 };
 
-void vlc_set_path(libvlc_instance_t *vlc_instance, libvlc_media_player_t *vlc_player, const char* path){
+static void vlc_set_path(libvlc_instance_t *vlc_instance, libvlc_media_player_t *vlc_player, const char* path){
     libvlc_media_t *media = libvlc_media_new_path(vlc_instance, path);
     libvlc_media_player_set_media(vlc_player, media);
     libvlc_media_release(media);
 };
 
-struct VlcContainer vlc_init(){
+static struct VlcContainer vlc_init(){
     libvlc_instance_t *vlc_instance = libvlc_new(0, NULL);
     libvlc_media_player_t *vlc_player = libvlc_media_player_new(vlc_instance);
     const struct VlcContainer vlc_container = {vlc_instance, vlc_player, NULL};
     return vlc_container;
 }
 
-void vlc_quit(libvlc_instance_t *vlc_instance, libvlc_media_player_t *vlc_player){
+static void vlc_quit(libvlc_instance_t *vlc_instance, libvlc_media_player_t *vlc_player){
     libvlc_media_player_stop(vlc_player);
     libvlc_media_player_release(vlc_player);
     libvlc_release(vlc_instance);
@@ -64,22 +86,28 @@ static void gtk_on_button_pause_clicked(GtkWidget *window, gpointer user_data){
 static void gtk_app_activate(GtkApplication* application, gpointer user_data){
 
     GtkWidget *window = gtk_application_window_new(application);
-    gtk_window_set_title(GTK_WINDOW(window), "VLC Player on GTK3");
+    gtk_window_set_title(GTK_WINDOW(window), "Phantom CPlayer");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
     //g_signal_connect(window, "destroy", G_CALLBACK(gtk_on_window_destroy), user_data);
 
     GtkWidget *box_window = gtk_button_box_new(GTK_ORIENTATION_VERTICAL);
+    gtk_widget_set_hexpand(box_window, true);
+    gtk_widget_set_vexpand(box_window, true);
+    gtk_widget_set_halign(box_window, GTK_ALIGN_FILL);
+    gtk_widget_set_valign(box_window, GTK_ALIGN_FILL);
     gtk_container_add(GTK_CONTAINER(window), box_window);
 
     GtkWidget *drawing_area = gtk_drawing_area_new();
     gtk_widget_set_size_request(drawing_area, 600, 400);
     gtk_widget_set_hexpand(drawing_area, true);
     gtk_widget_set_vexpand(drawing_area, true);
+    gtk_widget_set_halign(drawing_area, GTK_ALIGN_FILL);
+    gtk_widget_set_valign(drawing_area, GTK_ALIGN_FILL);
+
     gtk_container_add(GTK_CONTAINER(box_window), drawing_area);
     //gtk_box_pack_start(GTK_BOX(box_window), drawing_area, true, true, 0);
     g_signal_connect(drawing_area, "realize", G_CALLBACK(gtk_on_drawing_area_realize), user_data);
     g_signal_connect(drawing_area, "draw", G_CALLBACK(gtk_on_drawing_area_draw_enter), NULL);
-
 
     GtkWidget *box_controls = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_container_add(GTK_CONTAINER(box_window), box_controls);
@@ -101,7 +129,7 @@ int main(int argc, char* argv[]){
 
     struct VlcContainer vlc_container = vlc_init();
 
-    GtkApplication *application = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *application = gtk_application_new("com.senties-martinelli.PhantomCPlayer", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(application, "activate", G_CALLBACK(gtk_app_activate), &vlc_container);
 
     const int gtk_status = g_application_run(G_APPLICATION(application), argc, argv);
